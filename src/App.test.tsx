52b61f4f4617b575a85270038f05f1fc7f091c8a
Sam Aran <sam.erin.williams@gmail.com>
2024-11-12 14:52:56 -0800

    update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './components/App';
 
-test('renders learn react link', () => {
+test('renders homepage', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Bark Resume/i);
   expect(linkElement).toBeInTheDocument();
 });
